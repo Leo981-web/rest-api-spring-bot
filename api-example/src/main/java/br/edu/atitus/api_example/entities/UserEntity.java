56032dabdfe98888c1d1391367ab4dea1200babb
Leo981-web <leozancanaro@outlook.com.br>
2025-11-04 21:59:35 -0300

    Greeting Controller

---
+++ api-example/src/main/java/br/edu/atitus/api_example/entities/UserEntity.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.api_example.entities;
+
+import java.util.UUID;
+
+public class UserEntity {
+	
+	private UUID id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	
+}
